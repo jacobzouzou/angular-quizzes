070bbf1e11b57aca95028a38cf76656893fee6c2
jacobzouzou <jacob.zouzou@gmail.com>
2021-01-12 15:01:47 +0100

    Fix radio button binding issue

--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -6,6 +6,7 @@ import {AuthService} from "./services/auth.service"
   template: `
     <mat-toolbar>
       <button routerLink="/">Quiz</button>
+      <button routerLink="/play">Play Quiz</button>
       <span style="flex: 1 1 auto"></span>
       <button   *ngIf="!isAuthenticated" routerLink="/register">Register</button>
       <button  *ngIf="!isAuthenticated" routerLink="/login">Login</button>
