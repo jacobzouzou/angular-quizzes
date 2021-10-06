28ac0700044308c68a3cb9eb3d3d36fb4ec65e86
jacobzouzou <jacob.zouzou@gmail.com>
2021-10-06 13:59:12 +0200

    refact routing

---
+++ src/app/Route/app.routing.ts
@@ -0,0 +1,24 @@
+
+import { LoginComponent } from '../authentication/login.component';
+import { RegisterComponent } from '../authentication/register.component';
+import { PlayComponent } from '../Business/play.component';
+import { QuestionComponent } from '../Business/Questions/question.component';
+import { PlayQuizComponent } from '../Business/Quizzes/playQuiz.component';
+import { QuizComponent } from '../Business/Quizzes/quiz.component';
+import { HomeComponent } from '../Layout/home.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, //not need template url
+    { path: 'question', component: QuestionComponent },
+    { path: 'quizzes/:quizId/questions', component: QuestionComponent },
+    { path: 'quizzes/:quizId/questions/:questionId', component: QuestionComponent },
+    { path: 'quiz', component: QuizComponent },
+    { path: 'play', component: PlayComponent },
+    { path: 'playQuiz/:quizId', component: PlayQuizComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+  ];
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './Route/app.routing.module';
 
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './Business/Questions/question.component';
