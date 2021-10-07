89c9abc3995bdcf42c03885c9c6f99f6a4a3b2a9
jacobzouzou <jacob.zouzou@gmail.com>
2021-10-07 11:50:39 +0200

    add activatedRoute to user url parameter

--- src/app/Business/Questions/question.component.ts
+++ src/app/Business/Questions/question.component.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {ApiService} from "../../services/api.service";
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
 // import {Question} from "../services/data.service";
 
